32ee9db30b5e3e28ad2908211ab2361ba796a380
Kadyr Gullyyev <kadyr.gullyyew@gmail.com>
2025-08-11 00:36:02 +0200

    c

--- app/deps/products.py
+++ app/deps/products.py
@@ -1,5 +1,4 @@
 from decimal import Decimal
-from email.mime import image
 from typing import Annotated, List
 from fastapi import Form, UploadFile, File, Depends
 from slugify import slugify
@@ -9,7 +8,7 @@ from slugify import slugify
 class ExtraImageCreate:
     def __init__(
             self,
-            extra_image: Annotated[UploadFile, File()],
+            extra_image: Annotated[UploadFile, File(alias="extraImage")],
             order: Annotated[int, Form(ge=1)],
     ):
         self.extra_image = extra_image
@@ -19,14 +18,15 @@ class ExtraImageCreate:
 class ProductCreate:
     def __init__(
             self,
-            name_en: Annotated[str, Form(..., min_length=2, max_length=50)],
-            name_ro: Annotated[str, Form(..., min_length=2, max_length=50)],
-            description_en: Annotated[str, Form(..., min_length=2, max_length=50)],
-            description_ro: Annotated[str, Form(..., min_length=2, max_length=50)],
-            category_id: Annotated[int, Form(..., ge=1)],
-            price: Annotated[Decimal, Form(..., min_length=2, max_length=50)],
+            name_en: Annotated[str, Form(..., min_length=2, max_length=50, alias="nameEn")],
+            name_ro: Annotated[str, Form(..., min_length=2, max_length=50, alias="nameRo")],
+            description_en: Annotated[str, Form(..., min_length=2, max_length=50, alias="descriptionEn")],
+            description_ro: Annotated[str, Form(..., min_length=2, max_length=50, alias="descriptionRo")],
+            category_id: Annotated[int, Form(..., ge=1, alias="categoryId")],
+            price: Annotated[Decimal, Form()],
             image: Annotated[UploadFile, File(...)],
-            extra_images: Annotated[List[ExtraImageCreate] | None, Depends()] = None,
+            extra_image: Annotated[List[UploadFile] | None, File(alias="extraImage")] = None,
+            extra_image_order: Annotated[List[int] | None, Form(alias='extraImageOrder')] = None,
             stock: Annotated[int | None, Form(..., ge=1)] = None,
             status: Annotated[bool | None, Form(ge=1)] = None,
     ):
@@ -37,7 +37,11 @@ class ProductCreate:
         self.category_id = category_id
         self.price = price
         self.image = image
-        self.extra_images = extra_images
+        self.extra_images = []
+        if len(extra_image) != len(extra_image_order):
+            raise ValueError("Mismatch between number of extra images and orders")
+        for file, order in zip(extra_image, extra_image_order):
+            self.extra_images.append(ExtraImageCreate(file, order))
         self.stock = stock
         self.status = status
 
